8251b7aecf3eb9cdacee1b40fb34ea7eddc164c5
helloworldwdchen <helloworldchen@qq.com>
2020-03-20 16:51:07 +0800

    第一次优化定时器

--- day7/2/timer.c
+++ day7/2/timer.c
@@ -12,6 +12,7 @@ void init_pit(void){
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
+    timerctl.next = 0xffffffff;
     for(i=0; i<MAX_TIMER; i++){
         timerctl.timer[i].flags = 0;
     }
@@ -43,6 +44,10 @@ void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data){
 void timer_settime(struct TIMER *timer, unsigned int timeout){
     timer->timeout = timeout+timerctl.count;
     timer->flags = TIMER_FLAGS_USING;
+    if(timerctl.next>timer->timeout){
+        /* 更新下一次的时刻*/
+        timerctl.next = timer->timeout;
+    }
     return ;
 }
 
@@ -50,11 +55,22 @@ void inthandler20(int *esp){
     int i;
     io_out8(PIC0_OCW2, 0x60);   /* 把IRQ-00信号接收完了的信息通知给PIC */
     timerctl.count++;
+    if(timerctl.next>timerctl.count){
+        return; /* 还不到下一个时刻，所以结束*/
+    }
+    timerctl.next = 0xffffffff;
     for(i=0; i<MAX_TIMER; i++){
         if(timerctl.timer[i].flags == TIMER_FLAGS_USING){
             if(timerctl.timer[i].timeout <= timerctl.count){
+                /* 超时 */
                 timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
                 fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
+            }
+            else{
+                /* 还没有超时 */
+                if(timerctl.next > timerctl.timer[i].timeout){
+                    timerctl.next = timerctl.timer[i].timeout;
+                }
             }
         }
     }
