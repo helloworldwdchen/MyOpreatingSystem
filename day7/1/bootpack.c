6604ca461cd374b8ec098290552c3ed9af991c2d
helloworldwdchen <helloworldchen@qq.com>
2020-03-20 16:38:17 +0800

    初步完成定时器的开发

--- day7/1/bootpack.c
+++ day7/1/bootpack.c
@@ -5,9 +5,12 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
 
 void HariMain(void){
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;/* 与asmhead.nas保持一致 */
-	char s[40],keybuf[32],mousebuf[128];
+	struct FIFO8 timerfifo, timerfifo2, timerfifo3;
+	char s[40],keybuf[32],mousebuf[128], timerbuf[8], timerbuf2[8], timerbuf3[8];
+	struct TIMER *timer, *timer2, *timer3;
+	
 	int mx,my,i;
-	unsigned int memtotal,count = 0;
+	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN*) MEMMAN_ADDR;
 
@@ -15,15 +18,28 @@ void HariMain(void){
 	struct SHEET *sht_back, *sht_mouse, *sht_win;
 	unsigned char *buf_back, buf_mouse[256], *buf_win;
 
-
 	init_gdtidt();
 	init_pic();
-	io_sti(); 
+	io_sti(); 	/* IDT/PIC的初始化已经结束，所以解除CPU的中断禁止*/
 	fifo8_init(&keyfifo, 32, keybuf);
 	fifo8_init(&mousefifo, 128, mousebuf);
-	io_out8(PIC0_IMR, 0xf9);
-	io_out8(PIC1_IMR, 0xef);
+	init_pit();
+	io_out8(PIC0_IMR, 0xf8);	/* PIT和PIC1和键盘设置为许可(11111000) */
+	io_out8(PIC1_IMR, 0xef);	/* 鼠标设置为许可(11101111) */
 	
+	fifo8_init(&timerfifo, 8, timerbuf);
+	timer = timer_alloc();
+	timer_init(timer, &timerfifo, 1);
+	timer_settime(timer, 1000);
+	fifo8_init(&timerfifo2, 8, timerbuf2);
+	timer2 = timer_alloc();
+	timer_init(timer2, &timerfifo2, 1);
+	timer_settime(timer2, 300);
+	fifo8_init(&timerfifo3, 8, timerbuf3);
+	timer3 = timer_alloc();
+	timer_init(timer3, &timerfifo3, 1);
+	timer_settime(timer3, 50);
+
 	init_keyboard();
 	enable_mouse(&mdec);
 	memtotal = memtest(0x00400000, 0xbfffffff);
@@ -45,9 +61,7 @@ void HariMain(void){
 
 	init_screen(buf_back, binfo->scrnx, binfo->scrny);
 	init_mouse_cursor8(buf_mouse, 99);		/* 背景色号99 */
-	make_window8(buf_win, 160, 68, "counter");
-	//putfonts8_asc(buf_win, 160, 24, 28, COL8_000000, "Welcome to");
-	//putfonts8_asc(buf_win, 160, 24, 44, COL8_000000, "  Wdc-OS!");
+	make_window8(buf_win, 160, 52, "counter");
 	
 	sheet_slide(sht_back, 0, 0);
 	mx = (binfo->scrnx - 16)/2;		/* 按显示在画面中央来计算坐标 */
@@ -66,14 +80,14 @@ void HariMain(void){
 	sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
 
 	for(;;){
-		count++;
-		sprintf(s, "%010d", count);
+		sprintf(s, "%010d", timerctl.count);
 		boxfill8(buf_win, 160,COL8_C6C6C6, 40, 28, 119, 43);
 		putfonts8_asc(buf_win, 160, 40, 28, COL8_000000, s);
-		
 		sheet_refresh(sht_win, 40, 28, 120, 44);
+
 		io_cli();
-		if(fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0){
+		if(fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo)+
+			fifo8_status(&timerfifo2) + fifo8_status(&timerfifo3) == 0){
 			io_sti(); /* 不做HLT*/
 		}
 		else{
@@ -114,6 +128,32 @@ void HariMain(void){
 					sheet_refresh(sht_back, 0, 0, 80, 16);
 					sheet_slide(sht_mouse, mx, my);	/* 包含sheet_refresh含sheet_refresh */
 				}	
+			 }
+			 else if(fifo8_status(&timerfifo) != 0){
+				 i = fifo8_get(&timerfifo);
+				 io_sti();
+				 putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
+				 sheet_refresh(sht_back, 0, 64, 56, 80);
+			 }
+			 else if(fifo8_status(&timerfifo2) != 0){
+				 i = fifo8_get(&timerfifo2);
+				 putfonts8_asc(buf_back, binfo->scrnx, 0, 80, COL8_FFFFFF, "3[sec]");
+				 sheet_refresh(sht_back, 0, 80, 48, 96);
+			 }
+			 else if(fifo8_status(&timerfifo3) != 0){
+				 i = fifo8_get(&timerfifo3);
+				 io_sti();
+				 if(i!=0){
+					 timer_init(timer3, &timerfifo3, 0);
+
+					 boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+				 }
+				 else{
+					 timer_init(timer3, &timerfifo3, 1);
+					 boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+				 }
+				 timer_settime(timer3, 50);
+				 sheet_refresh(sht_back, 8, 96, 16, 112);
 			 }
 		}
 	}
